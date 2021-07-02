f98e361e4dc116df97a7959d8c3735bd73e69f75
DavidMatute <86425726+DavidMatute@users.noreply.github.com>
2021-07-01 20:25:42 -0600

    primer commit
    Creación de una matriz bidimensional

---
+++ Matriz/Matriz/frm_CrearMatriz.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Matriz
+{
+    partial class frm_CrearMatriz
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Filas = new System.Windows.Forms.TextBox();
+            this.txt_Columnas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Crear = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INGRESE LAS FILAS:";
+            // 
+            // txt_Filas
+            // 
+            this.txt_Filas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Filas.Location = new System.Drawing.Point(304, 40);
+            this.txt_Filas.Name = "txt_Filas";
+            this.txt_Filas.Size = new System.Drawing.Size(100, 29);
+            this.txt_Filas.TabIndex = 1;
+            // 
+            // txt_Columnas
+            // 
+            this.txt_Columnas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Columnas.Location = new System.Drawing.Point(304, 112);
+            this.txt_Columnas.Name = "txt_Columnas";
+            this.txt_Columnas.Size = new System.Drawing.Size(100, 29);
+            this.txt_Columnas.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "INGRESE LAS COLUMNAS:";
+            // 
+            // btn_Crear
+            // 
+            this.btn_Crear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Crear.Location = new System.Drawing.Point(44, 184);
+            this.btn_Crear.Name = "btn_Crear";
+            this.btn_Crear.Size = new System.Drawing.Size(98, 48);
+            this.btn_Crear.TabIndex = 4;
+            this.btn_Crear.Text = "Crear";
+            this.btn_Crear.UseVisualStyleBackColor = true;
+            this.btn_Crear.Click += new System.EventHandler(this.btn_Crear_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(172, 184);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(98, 48);
+            this.btn_Limpiar.TabIndex = 5;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = true;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(306, 184);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(98, 48);
+            this.btn_Salir.TabIndex = 6;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // frm_CrearMatriz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 306);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Crear);
+            this.Controls.Add(this.txt_Columnas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Filas);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_CrearMatriz";
+            this.Text = "Creación de una Matriz Bidimensional:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Filas;
+        private System.Windows.Forms.TextBox txt_Columnas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Crear;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+    }
+}
+
---
+++ Matriz/Matriz/frm_CrearMatriz.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matriz
+{
+    public partial class frm_CrearMatriz : Form
+    {
+        public frm_CrearMatriz()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Crear_Click(object sender, EventArgs e)
+        {
+            Crear_Matriz();
+        }
+
+        private void Crear_Matriz()
+        {
+            int Filas = Convert.ToInt32(txt_Filas.Text);
+            int Columnas = Convert.ToInt32(txt_Columnas.Text);
+            var randomNumber = new Random();
+
+            int[,] Crear_Matriz = new int[Filas, Columnas];
+
+            for (int i = 0; i < Crear_Matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < Crear_Matriz.GetLength(1); j++)
+                {
+                    Crear_Matriz[i, j] = randomNumber.Next(0, 100);
+
+                }
+            }
+
+            for (int i = 0; i < Crear_Matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < Crear_Matriz.GetLength(1); j++)
+                {
+
+                    MessageBox.Show($"La posición es  ({i},{j}) = {Crear_Matriz[i, j].ToString()}");
+                }
+            }
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            txt_Filas.Clear();
+            txt_Columnas.Clear();
+            txt_Filas.Focus();
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Salir();
+        }
+
+        private void Salir()
+        {
+            Close();
+        }
+    }
+}
